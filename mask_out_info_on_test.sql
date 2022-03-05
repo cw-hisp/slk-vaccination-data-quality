0f95261449e71eea6f1a7d7366455885d61b08a1
cw-hisp <77595189+cw-hisp@users.noreply.github.com>
2022-03-05 18:35:46 +0530

    adding test mask out info script

---
+++ mask_out_info_on_test.sql
@@ -0,0 +1,13 @@
+update trackedentityattributevalue
+set value='removed_on_test'
+where trackedentityattributeid in (17605, 19915,
+    17607,
+    17593,
+    24658,
+    17621,
+    357636,
+    357642,
+    17587,
+    357648,
+    357649,
+    50842561 )
